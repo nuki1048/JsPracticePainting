0aee60c3fbbb808bd6c54698d50c14028760d3e2
Yur1 <nik456618@gmail.com>
2023-02-10 08:10:16 +0200

    fix hoverEffect(i removed variables that were not used)

--- src/js/modules/hoverEffect.js
+++ src/js/modules/hoverEffect.js
@@ -1,32 +1,29 @@
 const hoverEffect = () => {
-	const imgBlock = document.querySelectorAll(".sizes-block"),
-		size = document.querySelectorAll(".sizes-block .size"),
-		priceStart = document.querySelectorAll(".sizes-block .starting-price"),
-		priceFinal = document.querySelectorAll(".sizes-block .final-price");
+  const imgBlock = document.querySelectorAll('.sizes-block');
 
-	function showImg(block) {
-		const img = block.querySelector("img"),
-			path = img.getAttribute("src").slice(0, -4) + `-1.png`;
-		img.setAttribute("src", path);
-		block.querySelectorAll("p:not(.sizes-hit)").forEach((item) => {
-			item.style.display = "none";
-		});
-	}
-	function hideImg(block) {
-		const img = block.querySelector("img"),
-			path = img.getAttribute("src").slice(0, -6) + `.png`;
-		img.setAttribute("src", path);
-		block.querySelectorAll("p:not(.sizes-hit)").forEach((item) => {
-			item.style.display = "block";
-		});
-	}
-	imgBlock.forEach((item) => {
-		item.addEventListener("mouseenter", () => {
-			showImg(item);
-		});
-		item.addEventListener("mouseleave", () => {
-			hideImg(item);
-		});
-	});
+  function showImg(block) {
+    const img = block.querySelector('img');
+    const path = `${img.getAttribute('src').slice(0, -4)}-1.png`;
+    img.setAttribute('src', path);
+    block.querySelectorAll('p:not(.sizes-hit)').forEach((item) => {
+      item.style.display = 'none';
+    });
+  }
+  function hideImg(block) {
+    const img = block.querySelector('img');
+    const path = `${img.getAttribute('src').slice(0, -6)}.png`;
+    img.setAttribute('src', path);
+    block.querySelectorAll('p:not(.sizes-hit)').forEach((item) => {
+      item.style.display = 'block';
+    });
+  }
+  imgBlock.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      showImg(item);
+    });
+    item.addEventListener('mouseleave', () => {
+      hideImg(item);
+    });
+  });
 };
 export default hoverEffect;
